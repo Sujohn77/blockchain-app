1dc614ed5488bb8dfd2fae10bc237b775ce3afd7
sujohn77 <stonebo0sh56@gmail.com>
2024-03-26 18:42:14 +0200

    feat: basic controller

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChatGateway } from './gateways/chat';
 import { SocketModule } from './modules/socket/socket.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [AppController],
   providers: [SocketModule, ChatGateway],
 })
 export class AppModule {}
