4904a6ab9fdc9fc5dd23ab114293a3eb2a868005
sujohn77 <stonebo0sh56@gmail.com>
2024-03-26 23:01:58 +0200

    fix: build

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,10 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new IoAdapter(app));
-  await app.listen(3001).then(() => console.log('Server is running on 3001'));
+  const port = process.env.PORT || 3001;
+  await app
+    .listen(port)
+    .then(() => console.log(`Server is running on ${port}!`));
 }
 
 bootstrap();
